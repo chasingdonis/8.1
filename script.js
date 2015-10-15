981499054dfc84b87bf95faffc9589c4dbb3f8c6
chasingdonis <cuixianning@hotmail.com>
2015-10-15 17:58:31 -0500

    editing

---
+++ script.js
@@ -0,0 +1,15 @@
+function myNumber (a,b) {
+var result = a*b;
+return result;
+}
+
+var lottery = window.prompt("Guess what are the lottery numbers tonight?");
+
+var x = myNumber(9,3);
+alert(x);
+
+var x = myNumber(2,8);
+alert(x);
+
+var x = myNumber(5,1);
+alert(x);
